a4415786f5696ab894b0acaddc4f11450dce815a
Taheri-Developers <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-01-20 21:02:48 +0530

    fixed changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,31 @@
 import "@/styles/globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import LoadingBar from "react-top-loading-bar";
 
 export default function App({ Component, pageProps }) {
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      setProgress(40);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setProgress(100);
+    });
+  });
   return (
     <>
+      <LoadingBar
+        color="#4f46e5"
+        progress={progress}
+        height={5}
+        className="rounded-full"
+        waitingTime={400}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
